e3ef5b6358f9968b6b00665a05747b09f0fce047
Thomas Leese <thomas@leese.io>
2016-10-14 08:50:52 +0100

    Update lexer.h

--- compiler/parsing/lexer.h
+++ compiler/parsing/lexer.h
@@ -2,8 +2,7 @@
 // Created by Thomas Leese on 13/03/2016.
 //
 
-#ifndef ACORN_LEXER_H
-#define ACORN_LEXER_H
+#pragma once
 
 #include <map>
 #include <string>
@@ -14,24 +13,27 @@
 
 namespace acorn {
 
+    /**
+     * Lexical Analysis. Takes source code input and provides a stream of
+     * tokens as output.
+     */
     class Lexer : public compiler::Pass {
     public:
-        explicit Lexer();
-
-        ~Lexer();
+        Lexer();
 
     private:
-        void setRule(Token::Rule rule, std::string regex);
-        void setKeywordRule(Token::Rule rule, std::string keyword);
-        void loadRules();
+        void set_rule(Token::Kind kind, std::string regex);
+        void set_keyword_rule(Token::Kind kind, std::string keyword);
+        void load_rules();
 
     public:
-        std::vector<Token *> tokenise(std::string filename);
+        /**
+         * Tokenise a file containing source code into a list of tokens.
+         */
+        std::vector<Token> tokenise(std::string filename);
 
     private:
-        std::map<Token::Rule, std::string> m_rules;
+        std::map<Token::Kind, std::string> m_rules;
     };
 
 }
-
-#endif // ACORN_LEXER_H
