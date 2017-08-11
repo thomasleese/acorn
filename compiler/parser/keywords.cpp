d47482f0a8a4f405f3466a8cdce788b0e0584fb8
Thomas Leese <thomas@leese.io>
2017-08-11 08:27:17 +0100

    Add a keywords module

---
+++ compiler/parser/keywords.cpp
@@ -0,0 +1,18 @@
+#include <set>
+
+#include "keywords.h"
+
+using std::set, std::string;
+
+static set<string> keywords = {
+    "let", "def", "type", "as", "while", "for", "in", "if", "else", "not",
+    "and", "or", "end", "continue", "break", "try", "except", "raise",
+    "finally", "from", "import", "return", "with", "yield", "async", "await",
+    "repeat", "unless", "mutable", "spawn", "ccall", "using", "new", "inout",
+    "protocol", "enum", "switch", "case", "default", "module", "builtin",
+    "class", "interface", "static", "public", "private", "protected", "goto"
+};
+
+bool acorn::parser::is_keyword(const string &name) {
+    return keywords.find(name) != keywords.end();
+}
---
+++ compiler/parser/keywords.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace acorn {
+
+    namespace parser {
+
+        bool is_keyword(const std::string &name);
+
+    }
+
+}
