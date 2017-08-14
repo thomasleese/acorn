d16874b3e1c03b332331b4b647365d0b49141519
Thomas Leese <thomas@leese.io>
2017-08-14 23:43:06 +0100

    Improve utils code

--- compiler/include/acorn/utils.h
+++ compiler/include/acorn/utils.h
@@ -5,10 +5,10 @@
 #pragma once
 
 #define return_if_null(thing) \
-    if (thing == nullptr) return;
+    if (thing == nullptr) { return; }
 
 #define return_if_false(thing) \
-    if (thing == false) return;
+    if (thing == false) { return; }
 
 #define return_if_null_type(node) \
     return_if_false(node->has_type())
@@ -17,10 +17,10 @@
     if (thing == nullptr) { push_llvm_value(nullptr); return; }
 
 #define return_null_if_null(thing) \
-    if (thing == nullptr) return nullptr;
+    if (thing == nullptr) { return nullptr; }
 
 #define return_null_if_false(thing) \
-    if (thing == false) return nullptr;
+    if (thing == false) { return nullptr; }
 
 #define return_null_if_has_errors(thing) \
     if ((thing).has_errors()) { return nullptr; }
