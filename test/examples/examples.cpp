9481b83a0b9ebb90dff6c22fd741c0ba931898a9
Thomas Leese <thomas@leese.io>
2018-03-14 13:15:32 +0000

    Check exit code when running tests

--- test/examples/examples.cpp
+++ test/examples/examples.cpp
@@ -4,7 +4,7 @@
 
 #include "acorn/compiler.h"
 
-bool compile_and_run(const std::string filename) {
+int compile_and_run(const std::string filename) {
     std::string full_filename = "test/examples/" + filename + ".acorn";
 
     std::string exe_filename = "test/examples/" + filename;
@@ -12,20 +12,20 @@ bool compile_and_run(const std::string filename) {
     acorn::compiler::Compiler compiler;
     if (compiler.parse_and_compile(full_filename) != 0) {
         std::remove(exe_filename.c_str());
-        return false;
+        return -1;
     }
 
     int exit_code = system(exe_filename.c_str());
 
     std::remove(exe_filename.c_str());
 
-    return exit_code == 0;
+    return exit_code;
 }
 
 SCENARIO("example programs") {
     GIVEN("a program which should compile") {
-        REQUIRE(compile_and_run("generics"));
-        REQUIRE(compile_and_run("minimal"));
-        REQUIRE(compile_and_run("records"));
+        REQUIRE(compile_and_run("generics") == 0);
+        REQUIRE(compile_and_run("minimal") == 0);
+        REQUIRE(compile_and_run("records") == 0);
     }
 }
